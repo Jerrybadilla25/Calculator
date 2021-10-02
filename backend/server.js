53a97020950e5fd07ef37cbe99a174e579e5621d
Jerry Badilla <jerrybadilla@gmail.com>
2021-10-02 15:12:26 -0600

    make it better

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+app.set('port', process.env.PORT || 4200);
+app.use(express.static('./Public'));
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.listen(app.get('port'), ()=>{
+    console.log(`server on port ${app.get('port')}`)
+});
